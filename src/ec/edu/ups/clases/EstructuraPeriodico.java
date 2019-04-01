5839cd388cdee2a2a16311289aeae95600b33c73
Fernanda <Fernanda@HP_Dv6>
2019-03-31 22:59:37 -0500

    Inicializacion de Proyecto

--- src/ec/edu/ups/clases/EstructuraPeriodico.java
+++ src/ec/edu/ups/clases/EstructuraPeriodico.java
@@ -7,7 +7,6 @@ public class EstructuraPeriodico {
     private String ubicacion;
     private String nombrePeriodico;
     private Date fecha;
-    private int numero;
 
     public void setUbicacion(String ubicacion) {
         this.ubicacion = ubicacion;
